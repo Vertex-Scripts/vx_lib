3bd64a2b81a117ddd6c1c16f31b69729e64b5318
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-03-25 21:27:16 +0100

    feat: add request anim, texture and ptfx asset

--- modules/streaming/client.lua
+++ modules/streaming/client.lua
@@ -1,3 +1,5 @@
+-- TODO: add timeouts
+
 vx.streaming = {}
 
 ---@param model number | string
@@ -20,6 +22,67 @@ function vx.streaming.requestModel(model)
    end
 
    return model
+end
+
+---@param set string
+---@return string
+function vx.streaming.requestAnimSet(set)
+   if HasAnimSetLoaded(animSet) then
+      return set
+   end
+
+   RequestAnimSet(animSet)
+   while not HasAnimSetLoaded(animSet) do
+      Citizen.Wait(0)
+   end
+
+   return set
+end
+
+---@param dict string
+---@return string
+function vx.streaming.requestAnimDict(dict)
+   if HasAnimDictLoaded(animDict) then
+      return dict
+   end
+
+   RequestAnimDict(animDict)
+   while not HasAnimDictLoaded(animDict) do
+      Citizen.Wait(0)
+   end
+
+   return dict
+end
+
+---@param dict string
+---@return string?
+function vx.streaming.requestTextureDict(dict)
+   if HasStreamedTextureDictLoaded(dict) then
+      return dict
+   end
+
+   RequestStreamedTextureDict(dict, false)
+   while not HasStreamedTextureDictLoaded(dict) do
+      Citizen.Wait(0)
+   end
+
+   return dict
+end
+
+---@param asset string
+---@return string?
+function vx.streaming.requestPtfxAsset(asset)
+   if HasNamedPtfxAssetLoaded(assetName) then
+      return asset
+   end
+
+
+   RequestNamedPtfxAsset(assetName)
+   while not HasNamedPtfxAssetLoaded(assetName) do
+      Citizen.Wait(0)
+   end
+
+   return asset
 end
 
 return vx.streaming
