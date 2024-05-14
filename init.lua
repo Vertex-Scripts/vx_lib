7cb90ea574e214a69f3f45a2221090e5539a73b6
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-05-14 15:43:35 +0200

    fix: missing functions for points

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
 local resourceName = GetCurrentResourceName()
 local context = IsDuplicityVersion() and "server" or "client"
 local export = exports["vx_lib"]
+local intervals = {}
 
 local function loadResourceFile(root, module)
    local dir = ("%s/%s"):format(root, module)
@@ -9,7 +10,6 @@ local function loadResourceFile(root, module)
 
    return root, chunk, shared
 end
-
 
 local function loadModule(self, module)
    local dir, chunk, shared = loadResourceFile("modules", module)
@@ -68,6 +68,54 @@ local vx = setmetatable({
    __index = call,
    __call = call
 })
+
+---@param callback function | number
+---@param interval? number
+---@param ... any
+function SetInterval(callback, interval, ...)
+   interval = interval or 0
+
+   if type(interval) ~= 'number' then
+      return error(('Interval must be a number. Received %s'):format(json.encode(interval --[[@as unknown]])))
+   end
+
+   local cbType = type(callback)
+   if cbType == 'number' and intervals[callback] then
+      intervals[callback] = interval or 0
+      return
+   end
+
+   if cbType ~= 'function' then
+      return error(('Callback must be a function. Received %s'):format(cbType))
+   end
+
+   local args, id = { ... }
+   Citizen.CreateThreadNow(function(ref)
+      id = ref
+      intervals[id] = interval or 0
+      repeat
+         interval = intervals[id]
+         Wait(interval)
+         callback(table.unpack(args))
+      until interval < 0
+      intervals[id] = nil
+   end)
+
+   return id
+end
+
+---@param id number
+function ClearInterval(id)
+   if type(id) ~= 'number' then
+      return error(('Interval id must be a number. Received %s'):format(json.encode(id --[[@as unknown]])))
+   end
+
+   if not intervals[id] then
+      return error(('No interval exists with id %s'):format(id))
+   end
+
+   intervals[id] = -1
+end
 
 _ENV.vx = vx
 require = vx.require
