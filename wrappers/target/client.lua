6c0767f67c18ab4f671d6a987ca336453d490038
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-05-24 09:35:02 +0200

    refactor: cleanup up target wrappers

--- wrappers/target/client.lua
+++ wrappers/target/client.lua
@@ -120,196 +120,106 @@ end
 -- Functions --
 ---------------
 
--- Vehicle --
-
-local function addGlobalVehicle_ox(options)
-   exports.ox_target:addGlobalVehicle(options)
-end
-
-local function addGlobalVehicle_qb(options)
-   exports["qb-target"]:AddGlobalVehicle(options)
-end
-
 ---@param options EntityTargetOptions
 function vx.target.addGlobalVehicle(options)
    local targetOptions = convertOptions(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = addGlobalVehicle_ox,
-      ["qb-target"] = addGlobalVehicle_qb,
+      ["ox_target"] = function() exports.ox_target:addGlobalVehicle(targetOptions) end,
+      ["qb-target"] = function() exports["qb-target"]:AddGlobalVehicle(targetOptions) end,
    })
 
-   caller(targetOptions)
+   caller()
    return targetOptions
-end
-
----@param options OxTargetEntity
-local function removeGlobalVehicle_ox(options)
-   exports.ox_target:removeGlobalVehicle(options.name)
-end
-
----@param options QbTargetOption
-local function removeGlobalVehicle_qb(options)
-   exports["qb-target"]:RemoveGlobalVehicle(options.label)
 end
 
 ---@param options OxTargetEntity | QbTargetOption
 function vx.target.removeGlobalVehicle(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = removeGlobalVehicle_ox,
-      ["qb-target"] = removeGlobalVehicle_qb,
+      ["ox_target"] = function() exports.ox_target:removeGlobalVehicle(options.name) end,
+      ["qb-target"] = function() exports["qb-target"]:RemoveGlobalVehicle(options.label) end,
    })
 
    caller(options)
-end
-
--- Player --
-
-local function addGlobalPlayer_ox(options)
-   exports.ox_target:addGlobalPlayer(options)
-end
-
-local function addGlobalPlayer_qb(options)
-   exports["qb-target"]:AddGlobalPlayer(options)
 end
 
 ---@param options EntityTargetOptions
 function vx.target.addGlobalPlayer(options)
    local targetOptions = convertOptions(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = addGlobalPlayer_ox,
-      ["qb-target"] = addGlobalPlayer_qb,
+      ["ox_target"] = function() exports.ox_target:addGlobalPlayer(targetOptions) end,
+      ["qb-target"] = function() exports["qb-target"]:AddGlobalPlayer(targetOptions) end,
    })
 
-   caller(targetOptions)
+   caller()
    return targetOptions
 end
 
-local function removeGlobalPlayer_ox(options)
-   exports.ox_target:removeGlobalPlayer(options.name)
-end
-
-local function removeGlobalPlayer_qb(options)
-   exports["qb-target"]:RemoveGlobalPlayer(options.label)
-end
-
----@param options EntityTargetOptions
+---@param options OxTargetEntity | QbTargetOption
 function vx.target.removeGlobalPlayer(options)
-   local targetOptions = convertOptions(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = removeGlobalPlayer_ox,
-      ["qb-target"] = removeGlobalPlayer_qb,
+      ["ox_target"] = function() exports.ox_target:removeGlobalPlayer(options.name) end,
+      ["qb-target"] = function() exports["qb-target"]:RemoveGlobalPlayer(options.label) end,
    })
 
-   caller(targetOptions)
-   return targetOptions
-end
-
--- Ped --
-
-local function addGlobalPed_ox(options)
-   exports.ox_target:addGlobalPed(options)
-end
-
-local function addGlobalPed_qb(options)
-   exports["qb-target"]:AddGlobalPed(options)
+   caller()
 end
 
 ---@param options EntityTargetOptions
 function vx.target.addGlobalPed(options)
    local targetOptions = convertOptions(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = addGlobalPed_ox,
-      ["qb-target"] = addGlobalPed_qb,
+      ["ox_target"] = function() exports.ox_target:addGlobalPed(targetOptions) end,
+      ["qb-target"] = function() exports["qb-target"]:AddGlobalPed(targetOptions) end,
    })
 
-   caller(targetOptions)
+   caller()
    return targetOptions
 end
 
-local function removeGlobalPed_ox(options)
-   exports.ox_target:removeGlobalPed(options.name)
-end
-
-local function removeGlobalPed_qb(options)
-   exports["qb-target"]:RemoveGlobalPed(options.label)
-end
-
----@param options EntityTargetOptions
+---@param options OxTargetEntity | QbTargetOption
 function vx.target.removeGlobalPed(options)
-   local targetOptions = convertOptions(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = removeGlobalPed_ox,
-      ["qb-target"] = removeGlobalPed_qb,
+      ["ox_target"] = function() exports.ox_target:removeGlobalPed(options.name) end,
+      ["qb-target"] = function() exports["qb-target"]:RemoveGlobalPed(options.label) end,
    })
 
-   caller(targetOptions)
-   return targetOptions
+   caller()
 end
-
-function addLocalEntity_ox(entities, options)
-   exports.ox_target:addLocalEntity(entities, options)
-end
-
--- TODO
--- function addLocalEntity_qb(entities, options)
---    exports["qb-target"]:AddLocalEntity(entities, options)
--- end
 
 ---@param entities number|number[]
 ---@param options EntityTargetOptions
 function vx.target.addLocalEntity(entities, options)
    local targetOptions = convertOptions(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = addLocalEntity_ox,
-      -- ["qb-target"] = addLocalEntity_qb,
+      ["ox_target"] = function() exports.ox_target:addLocalEntity(entities, targetOptions) end,
+      -- ["qb-target"] = function () end,
    })
 
-   caller(entities, targetOptions)
+   caller()
    return targetOptions
-end
-
--- Models --
-
-local function addModel_ox(models, options)
-   exports.ox_target:addModel(models, options)
-end
-
-local function addModel_qb(models, options)
-   options.models = models
-   exports["qb-target"]:AddTargetModel(models, options)
 end
 
 ---@param models string|string[]
 ---@param options TargetOptions
 function vx.target.addModel(models, options)
    local targetOptions = convertOptions(options)
-   local caller, target = vx.caller.createTargetCaller({
-      ["ox_target"] = addModel_ox,
-      ["qb-target"] = addModel_qb,
+   local caller = vx.caller.createTargetCaller({
+      ["ox_target"] = function() exports.ox_target:addModel(models, targetOptions) end,
+      ["qb-target"] = function()
+         targetOptions.models = models
+         exports["qb-target"]:AddTargetModel(models, targetOptions)
+      end,
    })
 
-   if target == "qb-target" then
-      targetOptions.models = models
-   end
-
-   caller(models, targetOptions)
+   caller()
    return targetOptions
-end
-
-local function removeModel_ox(options)
-   exports.ox_target:removeModel(options.name)
-end
-
-local function removeModel_qb(options)
-   vx.logger.info(options)
-   exports["qb-target"]:RemoveTargetModel(options.models, options.label)
 end
 
 ---@param options EntityTargetOptions
 function vx.target.removeModel(options)
    local caller = vx.caller.createTargetCaller({
-      ["ox_target"] = removeModel_ox,
-      ["qb-target"] = removeModel_qb,
+      ["ox_target"] = function() exports.ox_target:removeModel(options.name) end,
+      ["qb-target"] = function() exports["qb-target"]:RemoveTargetModel(options.models, options.label) end,
    })
 
    caller(options)
