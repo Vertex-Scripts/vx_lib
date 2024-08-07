ad99481dd8b6a1e42490282162cd639c8393b673
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-08-07 12:35:15 +0200

    fix: spawnPed not returning

--- modules/spawnPed/shared.lua
+++ modules/spawnPed/shared.lua
@@ -1,6 +1,5 @@
 local isServerSide = IsDuplicityVersion()
 
-
 ---@param type number
 ---@param model string|number
 ---@param coords vector3
@@ -8,33 +7,35 @@ local isServerSide = IsDuplicityVersion()
 ---@param isNetwork? boolean
 ---@param bScriptHostPed? boolean
 function vx.spawnPed(type, model, coords, heading, isNetwork, bScriptHostPed)
-    local function spawnPed()
-        return CreatePed(type, model, coords.x, coords.y, coords.z, heading, isNetwork or false, bScriptHostPed or false)
-    end
+   local function spawnPed()
+      return CreatePed(type, model, coords.x, coords.y, coords.z, heading, isNetwork or false, bScriptHostPed or false)
+   end
 
-    if isServerSide then
-        if type(model) == "string" then
-            model = joaat(model)
-        end
+   if isServerSide then
+      if type(model) == "string" then
+         model = joaat(model)
+      end
 
-        local ped = spawnPed()
-        local promise = promise.new()
+      local ped = spawnPed()
+      local promise = promise.new()
 
-        CreateThread(function()
-            while not DoesEntityExist(ped) do
-                Citizen.Wait(50)
-            end
+      CreateThread(function()
+         while not DoesEntityExist(ped) do
+            Citizen.Wait(50)
+         end
 
-            promise:resolve(NetworkGetNetworkIdFromEntity(ped))
-        end)
+         promise:resolve(NetworkGetNetworkIdFromEntity(ped))
+      end)
 
-        return Citizen.Await(promise)
-    end
+      return Citizen.Await(promise)
+   end
 
-    if not vx.streaming.requestModel(model) then
-        return 0
-    end
-    
-    local ped = spawnPed()
-    return ped
+   if not vx.streaming.requestModel(model) then
+      return 0
+   end
+
+   local ped = spawnPed()
+   return ped
 end
+
+return vx.spawnPed
