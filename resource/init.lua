1b118b277bca3d35fa8d9c78cb120421845c4f6d
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-08-31 17:04:21 +0200

    refactor: improve detecting libraries

--- resource/init.lua
+++ resource/init.lua
@@ -65,33 +65,41 @@ vx = setmetatable({
    end
 })
 
+function doesResourceExist(resourceName)
+   local state = GetResourceState(resourceName)
+   return state ~= "missing"
+end
+
 function isResourceStarted(resourceName)
    local state = GetResourceState(resourceName)
-
    return state == "started"
 end
 
-function getLibrary(value, map)
-   if value ~= "auto" then
-      local resourceName = map[value]
-      if not isResourceStarted(resourceName) then
-         error(("Resource '%s' is not started"):format(value))
+function getLibrary(type, value, map)
+   function findLibrary()
+      if value ~= "auto" then
+         local resourceName = map[value]
+         if not doesResourceExist(resourceName) then
+            error(("Resource '%s' is not started"):format(value))
+         end
+
+         return value
       end
 
-      return value
-   end
+      if map == targetResourceMap and doesResourceExist("ox_target") then
+         return "ox_target"
+      end
 
-   if map == targetResourceMap and isResourceStarted("ox_target") then
-      return "ox_target"
-   end
-
-   for system, resourceName in pairs(map) do
-      if isResourceStarted(resourceName) then
-         return system
+      for system, resourceName in pairs(map) do
+         if doesResourceExist(resourceName) then
+            return system
+         end
       end
    end
 
-   return nil
+   local result = findLibrary()
+   local isStarted = isResourceStarted(map[result])
+   print(("Using %s: ^2%s ^1%s^0"):format(type, result, not isStarted and "(Not started)" or ""))
 end
 
 local function initializeFramework(framework)
@@ -108,15 +116,11 @@ local function initializeFramework(framework)
    end
 end
 
-local framework = getLibrary(frameworkSystem, frameworkResourceMap) or error("Failed to get framework.")
-local inventory = getLibrary(inventorySystem, inventoryResourceMap) or error("Failed to get inventory")
-local target = getLibrary(targetSystem, targetResourceMap) or error("Failed to get target")
+local framework = getLibrary("framework", frameworkSystem, frameworkResourceMap)
+local inventory = getLibrary("inventory", inventorySystem, inventoryResourceMap)
+local target = getLibrary("target", targetSystem, targetResourceMap)
 initializeFramework(framework)
 
 exports("getFramework", function() return framework end)
 exports("getInventory", function() return inventory end)
 exports("getTarget", function() return target end)
-
-vx.print.info("Using framework", framework)
-vx.print.info("Using inventory", inventory)
-vx.print.info("Using target", target)
