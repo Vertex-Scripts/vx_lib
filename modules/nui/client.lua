a7a94a7c619d755d5a2fa8c79116376f21bbfc8d
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-09-01 09:28:29 +0200

    feat: add nui utilities

---
+++ modules/nui/client.lua
@@ -0,0 +1,69 @@
+-- TODO: Needs testing
+
+vx.nui = {}
+vx.registerCallback = RegisterNUICallback
+
+local registeredStores = {}
+
+function vx.nui.sendAction(action, data)
+   SendNUIMessage({
+      action = action,
+      data = data
+   })
+end
+
+---@generic T
+---@param name string
+---@param defaultValue T
+---@return { name: string, value: T, set: fun(self, val: T), onUpdate: fun(self, val: T) }
+function vx.nui.createStore(name, defaultValue)
+   if registeredStores[name] then
+      return registeredStores[name]
+   end
+
+   local store = {
+      name = name,
+      value = defaultValue
+   }
+
+   function store:set(value)
+      self.value = value
+
+      nui.sendAction("updateStore", {
+         name = self.name,
+         value = self.value
+      })
+
+      store:onUpdate(self.value)
+   end
+
+   function store:onUpdate(val) end
+
+   setmetatable(store, {
+      __gc = function()
+         registeredStores[store.name] = nil
+      end
+   })
+
+   registeredStores[store.name] = store
+   return store
+end
+
+function vx.nui.onInitialized(func)
+   nui.registerCallback("initialized", function(_, cb)
+      func()
+      cb()
+   end)
+end
+
+nui.registerCallback("getStoreValue", function(name, cb)
+   local store = registeredStores[name]
+   cb(store?.value)
+end)
+
+nui.registerCallback("setStoreValue", function(data, cb)
+   local store = registeredStores[data.name]
+   store:set(data.value)
+
+   cb()
+end)
