adb4945a6fbccc9f6237b97f21ecdbed6b470542
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-09-07 17:08:33 +0200

    refactor(points): extract interval functions to the module

--- init.lua
+++ init.lua
@@ -65,19 +65,6 @@ local function call(self, index, ...)
    return module
 end
 
-local function initializeFramework(framework)
-   local frameworkResourceName = frameworkResourceMap[framework]
-   if framework == "esx" then
-      _ENV.ESX = exports[frameworkResourceName]:getSharedObject()
-   elseif framework == "qb" then
-      _ENV.QBCore = exports[frameworkResourceName]:GetCoreObject()
-
-      RegisterNetEvent(("QBCore:%s:UpdateObject"):format(context), function()
-         _ENV.QBCore = exports[frameworkResourceName]:GetCoreObject()
-      end)
-   end
-end
-
 local vx = setmetatable({
    name = "vx_lib",
    systems = {
@@ -108,54 +95,16 @@ vx.cache = setmetatable({
    end or nil,
 })
 
----@param callback function | number
----@param interval? number
----@param ... any
-function SetInterval(callback, interval, ...)
-   interval = interval or 0
-
-   if type(interval) ~= 'number' then
-      return error(('Interval must be a number. Received %s'):format(json.encode(interval --[[@as unknown]])))
-   end
-
-   local cbType = type(callback)
-   if cbType == 'number' and intervals[callback] then
-      intervals[callback] = interval or 0
-      return
-   end
-
-   if cbType ~= 'function' then
-      return error(('Callback must be a function. Received %s'):format(cbType))
-   end
-
-   local args, id = { ... }
-   Citizen.CreateThreadNow(function(ref)
-      id = ref
-      intervals[id] = interval or 0
-      repeat
-         interval = intervals[id]
-         Wait(interval)
-         callback(table.unpack(args))
-      until interval < 0
-      intervals[id] = nil
-   end)
-
-   return id
-end
-
----@param id number
-function ClearInterval(id)
-   if type(id) ~= 'number' then
-      return error(('Interval id must be a number. Received %s'):format(json.encode(id --[[@as unknown]])))
-   end
-
-   if not intervals[id] then
-      return error(('No interval exists with id %s'):format(id))
-   end
-
-   intervals[id] = -1
-end
-
 _ENV.vx = vx
 require = vx.require
-initializeFramework(framework)
+
+local frameworkResourceName = frameworkResourceMap[framework]
+if framework == "esx" then
+   _ENV.ESX = exports[frameworkResourceName]:getSharedObject()
+elseif framework == "qb" then
+   _ENV.QBCore = exports[frameworkResourceName]:GetCoreObject()
+
+   RegisterNetEvent(("QBCore:%s:UpdateObject"):format(context), function()
+      _ENV.QBCore = exports[frameworkResourceName]:GetCoreObject()
+   end)
+end
