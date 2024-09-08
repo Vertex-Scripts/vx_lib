d753bd4107023804ef9395dcddab7fb9ba267fa5
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-09-08 08:44:52 +0200

    feat(typeCheck): support multiple types

--- modules/typeCheck/shared.lua
+++ modules/typeCheck/shared.lua
@@ -1,8 +1,17 @@
 ---@param value any
----@param expectedType type
-function vx.typeCheck(name, value, expectedType)
-   if type(value) ~= expectedType then
-      error(("expected %s to have type '%s' (received %s)"):format(name, expectedType, type(value)), 2)
+---@param expectedTypes type|type[]
+function vx.typeCheck(name, value, expectedTypes)
+   if type(expectedTypes) == "table" then
+      for _, expectedType in ipairs(expectedTypes) do
+         if type(value) == expectedType then return end
+      end
+
+      error(("expected %s to have type '%s' (received %s)"):format(name, table.concat(expectedTypes, ", "), type(value)),
+         2)
+   else
+      if type(value) ~= expectedTypes then
+         error(("expected %s to have type '%s' (received %s)"):format(name, expectedTypes, type(value)), 2)
+      end
    end
 end
 
