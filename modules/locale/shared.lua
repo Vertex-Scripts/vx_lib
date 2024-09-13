28341d6a8e79d23f79bf2b3ed2bd918b94475c7d
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-09-13 17:19:22 +0200

    feat(locale): flatten dictionary

--- modules/locale/shared.lua
+++ modules/locale/shared.lua
@@ -16,6 +16,19 @@ vx.locale = setmetatable({}, {
    end
 })
 
+local function flattenDict(input, target, prefix)
+   for k, v in pairs(input) do
+      local finalKey = prefix and ("%s.%s"):format(prefix, k) or k
+      if type(v) == "table" then
+         flattenDict(v, target, finalKey)
+      else
+         target[finalKey] = v
+      end
+   end
+
+   return target
+end
+
 local function loadLocale(locale)
    local file = LoadResourceFile(vx.cache.resource, ("locales/%s.json"):format(locale))
    if not file then
@@ -34,7 +47,7 @@ function vx.locale.load(key)
    local locales = loadLocale(key)
    table.wipe(dict)
 
-   for k, v in pairs(locales) do
+   for k, v in pairs(flattenDict(locales, {})) do
       for var in v:gmatch('${[%w%s%p]-}') do
          local locale = locales[var:sub(3, -2)]
 
