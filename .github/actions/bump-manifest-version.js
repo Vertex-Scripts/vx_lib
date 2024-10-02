21190adadd550fd6759239ad7ee054622d1639b0
Thomas <40025225+thoo0224@users.noreply.github.com>
2024-10-02 09:53:19 +0200

    fix: use " instead of ' in manifest version

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -4,6 +4,6 @@ const version = process.env.TGT_RELEASE_VERSION
 const newVersion = version.replace('v', '')
 
 const manifestFile = fs.readFileSync('fxmanifest.lua', { encoding: 'utf8' })
-const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version '${newVersion}'`)
+const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version "${newVersion}"`)
 
 fs.writeFileSync('fxmanifest.lua', newFileContent)
