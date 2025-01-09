3a479a8212264c3303dce7806c326619b818d7ec
SlothsAreLazyTho <chinobman@gmail.com>
2025-01-09 12:57:24 +0100

    Added protection when notify executes from the console

--- resource/interface/server/notify.lua
+++ resource/interface/server/notify.lua
@@ -1,5 +1,8 @@
 ---@param options NotificationOptions
 ---@diagnostic disable-next-line: duplicate-set-field
 function vx.notify(source, options)
+   if source < 1 then
+      return
+   end
    TriggerClientEvent("vx_lib:notify", source, options)
 end
