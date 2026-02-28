656042ffca6c605696388d2955f0392997a3ddfb
Thomas <40025225+thoo0224@users.noreply.github.com>
2026-02-28 03:12:11 +0100

    feat(events): add some events

--- modules/events/server.lua
+++ modules/events/server.lua
@@ -8,4 +8,22 @@ function vx.events.onPlayerLeave(cb)
    end)
 end
 
+--- @param cb fun(playerId: number, name: string, setKickReason: fun(reason: string), deferrals: any)
+function vx.events.onPlayerConnecting(cb)
+   vx.addEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+      local source = source
+      cb(source, name, setKickReason, deferrals)
+   end)
+end
+
+---@param cb fun(resourceName: string)
+function vx.events.onResourceStart(cb)
+   vx.addEventHandler("onResourceStart", cb)
+end
+
+---@param cb fun(resourceName: string)
+function vx.events.onResourceStop(cb)
+   vx.addEventHandler("onResourceStop", cb)
+end
+
 return vx.events
